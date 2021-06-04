a911d6be216f99aa5e0c3679a7a0bc142fa2f2d8
Maximous Black <maximousblk@gmail.com>
2021-06-04 14:55:00 +0530

    chore(deps): bump std to `0.97.0` (#3)

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export { extname, posix } from "https://deno.land/std@0.74.0/path/mod.ts";
-export { listenAndServe } from "https://deno.land/std@0.74.0/http/server.ts";
+export { extname, posix } from "https://deno.land/std@0.97.0/path/mod.ts";
+export { listenAndServe } from "https://deno.land/std@0.97.0/http/server.ts";
 export type {
   Response,
   ServerRequest,
-} from "https://deno.land/std@0.74.0/http/server.ts";
-export { parse } from "https://deno.land/std@0.74.0/flags/mod.ts";
-export { assert } from "https://deno.land/std@0.74.0/_util/assert.ts";
+} from "https://deno.land/std@0.97.0/http/server.ts";
+export { parse } from "https://deno.land/std@0.97.0/flags/mod.ts";
+export { assert } from "https://deno.land/std@0.97.0/_util/assert.ts";
